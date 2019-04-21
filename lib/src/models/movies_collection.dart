1ccbe351102b67dae5772061addf7856cf827df9
m_malujdy <natri4x@gmail.com>
2019-04-21 14:09:00 +0100

    Cleanup

--- lib/src/models/movies_collection.dart
+++ lib/src/models/movies_collection.dart
@@ -8,8 +8,6 @@ class MoviesCollection {
 
   MoviesCollection.fromJson(Map<String, dynamic> parsedJson) {
     page = parsedJson['page'];
-    // final total_results = parsedJson['total_results'];
-    // final total_pages = parsedJson['total_pages'];
 
     final int resultsCount = parsedJson['results'].length;
     for (var i = 0; i < resultsCount; i++) {
