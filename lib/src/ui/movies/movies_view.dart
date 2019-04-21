aee3879869cbaf2d016b7cc6d7317586f8555b79
m_malujdy <natri4x@gmail.com>
2019-04-21 13:54:53 +0100

    Display posters

--- lib/src/ui/movies/movies_view.dart
+++ lib/src/ui/movies/movies_view.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:movies/src/ui/movies/movies_gallery.dart';
+import 'package:movies/src/blocs/movies_bloc.dart';
+import 'package:movies/src/models/movies_collection.dart';
 
 class MoviesView extends StatelessWidget {
+  final _moviesBloc = MoviesBloc();
+  get moviesStream => _moviesBloc.movies;
+
+  MoviesView() {
+    //_moviesBloc.getAllMovies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +16,50 @@ class MoviesView extends StatelessWidget {
       appBar: new AppBar(
         title: Text('Movies'),
       ),
-      body: MoviesGallery()
+      body: _buildBody()
+    );
+  }
+
+  StreamBuilder<MoviesCollection> _buildBody() {
+    
+    _moviesBloc.getAllMovies();
+    return StreamBuilder(
+      stream: moviesStream,
+      builder: (context, AsyncSnapshot<MoviesCollection> snapshot) {
+        if (snapshot.hasData) {
+          return _buildGallery(snapshot);
+        }
+        else if(snapshot.hasError) {
+          return Text(snapshot.error.toString());
+        }
+        
+        return Center(
+          child: CircularProgressIndicator()
+        );
+      },
+    );
+  }
+
+  Widget _buildGallery(AsyncSnapshot<MoviesCollection> snapshot) {
+    return GridView.builder(
+      itemCount: snapshot.data.movies.length,
+      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return getPosterOverNetwork(snapshot, index);
+      },
+    );
+  }
+
+  getPosterOverNetwork(AsyncSnapshot<MoviesCollection> snapshot, int index) {
+    final baseUri = 'https://image.tmdb.org/t/p/w185';
+    final posterUri = snapshot.data.movies[index].posterPath;
+    final fullPosterUrl = baseUri + posterUri;
+
+    return Image.network(
+      fullPosterUrl,
+      fit: BoxFit.cover
     );
   }  
 }
